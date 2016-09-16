1c22670f898220e69cee5b9338f6518d85cc6704
Barry Fogarty <barry.fogarty@thisisdare.com>
2016-09-16 15:02:31 +0100

    File Upload WebAPI controller

---
+++ Controllers/FileUploadApiController.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using Umbraco.Web.Mvc;
+using Umbraco.Web.WebApi;
+
+namespace SimpleFileUpload
+{
+    [PluginController("SimpleFileUpload")]
+    public class FileUploadApiController : UmbracoAuthorizedApiController
+    {
+        public async Task<HttpResponseMessage> UploadFileToServer()
+        {
+            if (!Request.Content.IsMimeMultipartContent())
+            {
+                throw new HttpResponseException(HttpStatusCode.UnsupportedMediaType);
+            }
+
+            // Make this directory whatever makes sense for your project.
+            var root = HttpContext.Current.Server.MapPath("~/App_Data/Temp/SimpleFileUpload");
+            Directory.CreateDirectory(root);
+            var provider = new CustomMultipartFormDataStreamProvider(root);
+            var result = await Request.Content.ReadAsMultipartAsync(provider);
+
+            if (result.FileData.FirstOrDefault() == null)
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, "Error uploading file");
+
+            // Get full file path from disk
+            var filename = result.FileData.FirstOrDefault().LocalFileName;
+
+            // Return the feedback to the response
+            return Request.CreateResponse(HttpStatusCode.OK, filename);
+        }
+
+    }
+}
